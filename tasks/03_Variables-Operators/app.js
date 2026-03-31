2f7f082b9dfc3330fb8094640194771ec47815b7
th-cs <202718515+th-cs@users.noreply.github.com>
2026-03-31 01:08:30 -0300

    feat: Add variables and operators exercise

---
+++ tasks/03_Variables-Operators/app.js
@@ -0,0 +1,18 @@
+let lunch = "Pizza"
+let slices = 2
+let flavour = "Frango com Catupiry";
+
+console.log("Ela comeu " + slices + " fatias de " + lunch + " de " + flavour);
+
+slices = 4;
+flavour = "Calabresa com mussarela";
+
+console.log(`Eu comi ${slices} fatias de ${lunch} de ${flavour}`);
+
+let x = 10;
+let y = 2;
+
+console.log(`\nSoma: ${x} + ${y} = ${x + y}`);
+console.log(`Subtração: ${x} - ${y} = ${x - y}`);
+console.log(`Multiplicação: ${x} x ${y} = ${x * y}`);
+console.log(`Divisão: ${x} / ${y} = ${x / y}`);
