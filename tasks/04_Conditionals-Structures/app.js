0a67230194c00f702f67803e229f04b1ae16d323
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-05 21:18:49 -0300

    feat: Add conditional structures exercise

---
+++ tasks/04_Conditionals-Structures/app.js
@@ -0,0 +1,44 @@
+let month = 4;
+
+switch (month) {
+	case 1 :
+		console.log("Estamos no mês de Janeiro!");
+		break;
+	case 2 :
+		console.log("Estamos no mês de Fevereiro!");
+		break;
+	case 3 :
+		console.log("Estamos no mês de Março!");
+		break;
+	case 4 :
+		console.log("Estamos no mês de Abril!");
+		break;
+	case 5 :
+		console.log("Estamos no mês de Maio!");
+		break;
+	case 6 :
+		console.log("Estamos no mês de Junho!");
+		break;
+	case 7 :
+		console.log("Estamos no mês de Julho!");
+		break;
+	case 8 :
+		console.log("Estamos no mês de Agosto!");
+		break;
+	case 9 :
+		console.log("Estamos no mês de Setembro!");
+		break;
+	case 10 :
+		console.log("Estamos no mês de Outubro!");
+		break;
+	case 11 :
+		console.log("Estamos no mês de Novembro!");
+		break;
+	case 12 :
+		console.log("Estamos no mês de Dezembro!");
+		break;
+
+	default :
+		console.log("Valor não aceito!");
+		break;
+}
