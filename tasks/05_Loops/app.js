c982f97b263feb939e7d5bb55c364abbff7d1602
th-cs <202718515+th-cs@users.noreply.github.com>
2026-04-18 21:12:35 -0300

    feat: Add loops exercise

---
+++ tasks/05_Loops/app.js
@@ -0,0 +1,14 @@
+console.log("-- Contagem de 1 a 10 (Loop FOR) --");
+
+for (let i = 1; i <= 10; i++) {
+	console.log(`Contando: ${i}`);
+}
+
+let k = 2;
+
+console.log("\n-- Pares entre 2 e 20 (Loop WHILE) --");
+
+while (k <= 20) {
+	console.log(`Contando: ${k}`);
+	k += 2;
+}
